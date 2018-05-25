09d9c87823cd2d5c7e144b9b9aa40b196ff78a07
Julie K <julieskim160@gmail.com>
2018-05-25 08:15:31 -0700

    api cat endpoint

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ app.use(
   })
 );
 
+api.get('api/cat', (req, res) => {
+  res.json({
+      imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+      imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+      name: 'Fluffy',
+      sex: 'Female',
+      age: 2,
+      breed: 'Bengal',
+      story: 'Thrown on the street'
+  })
+
+})
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
